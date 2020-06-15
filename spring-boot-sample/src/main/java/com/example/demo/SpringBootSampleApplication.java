67c80204ca883dcf81c045daa709b3c3b7748daa
andigylle23 <kyriecamacho@gmail.com>
2020-06-15 15:00:02 +0900

    Settings for connecting to mysql

--- spring-boot-sample/src/main/java/com/example/demo/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/example/demo/SpringBootSampleApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //This annotation is used to mark a configuration class that declare one or more
 // @Bean methods and also triggers auto-configuration and component scanning.
 @SpringBootApplication
+@EnableJpaRepositories("com.example.demo.repository")
+@EntityScan("com..example.demo.model")
 public class SpringBootSampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
