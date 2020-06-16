1971bc9cb75486a7927ad9c7a134649395d7778b
andigylle23 <kyriecamacho@gmail.com>
2020-06-16 14:49:38 +0900

    Add simple pagination and updating the UI

--- spring-boot-sample/src/main/java/com/example/demo/controller/SimpleController.java
+++ spring-boot-sample/src/main/java/com/example/demo/controller/SimpleController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.exceptions.ResourceNotFoundException;
+import com.example.demo.model.Books;
 import com.example.demo.service.BookService;
 
 
@@ -20,17 +27,36 @@ public class SimpleController {
 		this.bookService = bookService;
 	}
 	
-	@GetMapping("/")
-	public  String homeDisplay(Model model) {
-		model.addAttribute("book", bookService.findAllRegisteredBooks());
+	@GetMapping("/books")
+	public  String homeDisplay(Model model,  @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
+		int currentPage = page.orElse(1);
+		int pageSize = size.orElse(5);
+		
+		Page<Books> bookPage = bookService.findPaginatedPage(PageRequest.of(currentPage - 1, pageSize));
+		
+		model.addAttribute("book", bookPage);
+		
+		int totalPages  = bookPage.getTotalPages();
+		if(totalPages > 0) {
+			List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+					.boxed()
+					.collect(Collectors.toList());
+			model.addAttribute("pageNumbers", pageNumbers);
+		}
 		return "index";
 	}
 	
-	@GetMapping("/delete/{title}")
+	@GetMapping("books/delete/{title}")
 	private String deleteBook(@PathVariable("title") String title) throws ResourceNotFoundException {
 		
 		bookService.deleteBookByTitle(title);
 		
-		return "redirect:/";
+		return "redirect:/books";
 	}
+	
+	@GetMapping("/")
+	private String redirectToBookLink() {
+		return "redirect:/books";
+	}
+	
 }
--- spring-boot-sample/src/main/java/com/example/demo/service/BookService.java
+++ spring-boot-sample/src/main/java/com/example/demo/service/BookService.java
@@ -1,10 +1,16 @@
 package com.example.demo.service;
 
+import java.util.Collections;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 import com.example.demo.exceptions.ResourceNotFoundException;
 import com.example.demo.model.Books;
 import com.example.demo.repository.BookRepository;
+
 
 @Service
 public class BookService {
@@ -28,8 +34,29 @@ public class BookService {
 		
 	}
 	
-	public Iterable<Books> findAllRegisteredBooks() {
+	public List<Books> findAllRegisteredBooks() {
 		return bookRepository.findAll();
+		
+	}
+	
+	
+	public Page<Books> findPaginatedPage(Pageable pageable) {
+		int pageSize = pageable.getPageSize();
+		int currentPage = pageable.getPageNumber();
+		int startItem = currentPage * pageSize;
+		List<Books> bookList;
+		List<Books> books = findAllRegisteredBooks();
+		
+		if(books.size() < startItem) {
+			bookList = Collections.emptyList();
+		} else {
+			int toIndex = (int) Math.min(startItem + pageSize, books.size());
+			bookList = books.subList(startItem, toIndex);
+		}
+		
+		Page<Books> bookPage = new PageImpl<Books>(bookList, PageRequest.of(currentPage, pageSize), books.size());
+		
+		return bookPage;
 		
 	}
 }
